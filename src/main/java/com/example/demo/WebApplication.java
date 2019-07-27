68b62733ef14ee19de6d813fc3e27d59a06d4da5
SatishJavaDeveloper <satishreddy270694@gmail.com>
2019-07-27 23:44:24 +0530

    commit changes

--- src/main/java/com/example/demo/WebApplication.java
+++ src/main/java/com/example/demo/WebApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
